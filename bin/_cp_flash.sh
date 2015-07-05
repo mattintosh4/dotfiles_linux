60e7f6a9275e4e92f7e3528497babf4261abf278
mattintosh4 <mattintosh4@gmail.com>
2015-07-05 10:20:19 +0900

    .

--- bin/_cp_flash.sh
+++ bin/_cp_flash.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+DSTROOT=${HOME}
+
+case ${1} in
+-d)
+    DSTROOT=.
+;;
+esac
+
 ifs=${IFS}
 IFS='
 '
@@ -10,7 +18,7 @@ for f
 do
     src=`echo ${f} | cut -d' ' -f9`
     dst=`echo ${f} | cut -d' ' -f11`
-    dst=${HOME}/`date +%Y%m%d%H%M%S`_${dst##*/}.mp4
+    dst=${DSTROOT}/`date +%Y%m%d%H%M%S`_${dst##*/}.mp4
     printf '%s -> %s\n' ${src} ${dst}
-    dd if=${src} of=${dst}
+    dd if=${src} of=${dst} bs=24M
 done
